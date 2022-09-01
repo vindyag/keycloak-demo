4a60e7c6a87e75430b89c1900d92fd565c314add
Vindya Lakimali Gunawardena <99x-vindyag@driw.no>
2022-09-01 12:28:23 +0530

    add keycloak auth

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import Keycloak from 'keycloak-js'
 
-createApp(App).mount('#app')
+let initOptions = {
+    url: 'http://localhost:8080/',
+    realm: 'demo',
+    clientId: 'my-vue-app',
+    onLoad: 'login-required'
+}
+
+let keycloak = new Keycloak(initOptions)
+keycloak
+    .init(
+        {
+            onLoad: initOptions.onLoad
+        }
+    )
+    .then(
+        (auth) => {
+            if (auth) {
+                console.log("Authenticated")
+                createApp(App).mount('#app')
+            } else {
+                console.error("Authentication failure")
+                window.location.reload()
+            }
+        })
+    .catch(
+        () => {
+            console.error("Error while trying to login")
+        })
