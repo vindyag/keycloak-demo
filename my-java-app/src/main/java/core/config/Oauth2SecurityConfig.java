da2709c2e59cbc5fcade2acc1e6773fa76ec15ee
vindyag <vindyag@99x.io>
2024-01-28 14:53:42 +0100

    upgrade versions

--- my-java-app/src/main/java/core/config/Oauth2SecurityConfig.java
+++ my-java-app/src/main/java/core/config/Oauth2SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -17,13 +20,17 @@ public class Oauth2SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(Customizer.withDefaults()) // Enable CORS support
-                .csrf().disable()
-                .authorizeHttpRequests(authorize ->
-                        authorize
-                                .requestMatchers(HttpMethod.OPTIONS).permitAll() // Allow pre-flight requests
-                                .requestMatchers("/actuator/health/**").permitAll() // Allow pre-flight requests
-                                .anyRequest().authenticated() // authenticate all other requests
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorizeRequests ->
+                                authorizeRequests
+                                        .requestMatchers(HttpMethod.OPTIONS).permitAll() // Allow pre-flight requests
+                                        .requestMatchers("/actuator/health/**").permitAll() // Allow app health requests
+                        .anyRequest().authenticated() // authenticate all other requests
+
                 );
+        http.oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(Customizer.withDefaults()));
 
         return http.build();
     }
